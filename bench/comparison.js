f790028428a6e2354992542b165474e89c6e1e61
Vas Sudanagunta <vas@commonkarma.org>
2020-04-29 00:06:21 -0400

    Fix benchmark to make randomly distributed index queries
    Previous version query the EXACT same text offset each
    and every time Benchmark.js called queried for its line
    and column. Now a list of randomly distributed index
    positions is created in advance, so that the same
    sequence of queries is applied to all the libraries
    being compared.

--- bench/comparison.js
+++ bench/comparison.js
@@ -16,73 +16,130 @@ const vscode = require("vscode-textbuffer")
 const lineColumn = require("line-column")
 const licofi = require("../dist").LineColumnFinder
 
+
 const candidates = [
     {
         name: "find-line-column",
-        lineAndCol: (text, target) => {
-            return () => findLineColumn(text, target)
+        lineAndCol: (text, stochasticOffsets) => {
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                findLineColumn(text, stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "simple-text-buffer",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const buff = new textBuffer(text)
-            return () => buff.positionForCharacterIndex(target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                buff.positionForCharacterIndex(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "char-props",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const cp = charProps(text)
-            return () => ({ line: cp.lineAt(target), column: cp.columnAt(target) })
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                cp.lineAt(stochasticOffsets[next])
+                cp.columnAt(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "string-pos",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             stringPos(text.short, 0)
-            return () => stringPos(text, target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                stringPos(text, stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "vfile-location",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const vfl = vfileLocation(vfile(text))
-            return () => vfl.toPosition(target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                vfl.toPosition(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "lines-and-columns",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const lac = new linesAndColumns(text)
-            return () => lac.locationForIndex(target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                lac.locationForIndex(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "vscode-textbuffer",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const pieceTreeTextBufferBuilder = new vscode.PieceTreeTextBufferBuilder()
             pieceTreeTextBufferBuilder.acceptChunk(text)
             const pieceTreeFactory = pieceTreeTextBufferBuilder.finish(true)
             const pieceTree = pieceTreeFactory.create(1)
-            return () => pieceTree.getPositionAt(target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                pieceTree.getPositionAt(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "line-column",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const lc = lineColumn(text)
-            return () => lc.fromIndex(target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                lc.fromIndex(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     },
     {
         name: "licofi",
-        lineAndCol: (text, target) => {
+        lineAndCol: (text, stochasticOffsets) => {
             const lcf = new licofi(text)
-            return () => lcf.fromIndex(target)
+            let len = stochasticOffsets.length
+            let next = 0
+            return function() {
+                lcf.fromIndex(stochasticOffsets[next])
+                next++
+                if (next === len) next = 0
+            }
         }
     }
 ]
+
 
 const createSuite = function (name, options) {
     return Benchmark.Suite(name, options).on("start", function () {
@@ -93,6 +150,12 @@ const createSuite = function (name, options) {
         return benchmarks.log()
     })
 }
+
+
+function randomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 
 const textCases = [
     {
@@ -115,6 +178,7 @@ const textCases = [
 
 const loremIpsum = "Lorem ipsum dolor sit amet\nconsectetur adipiscing elit.\nSuspendisse id sem vel mi cursus facilisis vel ac arcu.\nNulla vulputate tortor eu ipsum bibendum rhoncus"
 
+
 for (let j = 0, len1 = textCases.length; j < len1; j++) {
     const textCase = textCases[j]
     const text = ((function () {
@@ -126,11 +190,17 @@ for (let j = 0, len1 = textCases.length; j < len1; j++) {
     })()).join("\n")
     const len = text.length
     const lines = stringPos(text, len).line
-    const target = Math.round(text.length / 2 + ((text.length / 2) * 0.34))
+
+    const numOffsets = Math.min(text.length * 2, 50000)
+    const stochasticOffsets = new Array(numOffsets)
+    for (let m = 0; m < numOffsets; m++) {
+        stochasticOffsets[m] = randomInt(0, text.length-1)
+    }
+
     const suite = createSuite(textCase.name + " text: " + len + " chars, " + lines + " lines")
     for (let k = 0, len2 = candidates.length; k < len2; k++) {
         const c = candidates[k]
-        suite.add(c.name, c.lineAndCol(text, target))
+        suite.add(c.name, c.lineAndCol(text, stochasticOffsets))
     }
     suite.run()
 }
